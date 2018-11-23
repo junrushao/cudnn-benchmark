e3675e88ec5378c9a8fcc51bb89cbacbf8f63f1a
Junru Shao <junrushao1994@gmail.com>
2018-11-22 22:08:25 -0500

    Verify for LSTM and GRU as well

--- verify.py
+++ verify.py
@@ -5,10 +5,17 @@ def relu(x):
   return x * (x > 0)
 
 
+def sigmoid(x):
+  return 1.0 / (1.0 + np.exp(-x))
+
+
 def linear(x, w, b):
+  assert x.ndim == 1
   if w is not None:
+    assert w.ndim == 2
     x = w @ x
   if b is not None:
+    assert b.ndim == 1
     x = x + b
   return x
 
@@ -23,6 +30,23 @@ def tanh_cell(w, b, x, h, c):
   x = linear(x, w[0], b[0])
   h = linear(h, w[1], b[1])
   return np.tanh(x + h), c
+
+
+def gru_cell(w, b, x, h, c):
+  r = sigmoid(linear(x, w[0], b[0]) + linear(h, w[3], b[3]))
+  i = sigmoid(linear(x, w[1], b[1]) + linear(h, w[4], b[4]))
+  hp = np.tanh(linear(x, w[2], b[2]) + r * linear(h, w[5], b[5]))
+  return (1.0 - i) * hp + i * h, c
+
+
+def lstm_cell(w, b, x, h, c):
+  i = sigmoid(linear(x, w[0], b[0]) + linear(h, w[4], b[4]))
+  f = sigmoid(linear(x, w[1], b[1]) + linear(h, w[5], b[5]))
+  cp = np.tanh(linear(x, w[2], b[2]) + linear(h, w[6], b[6]))
+  o = sigmoid(linear(x, w[3], b[3]) + linear(h, w[7], b[7]))
+  c = f * c + i * cp
+  h = o * np.tanh(c)
+  return h, c
 
 
 def run(cell, w, b, seq, h, c):
@@ -122,5 +146,118 @@ def verify_tanh_cell():
   np.testing.assert_allclose(np_cy, cy, rtol=1e-6, atol=1e-6)
   print("Verify tanh cell done!")
 
+
+def verify_gru_cell():
+  # =========================================================
+  w = [None] * 6
+  b = [None] * 6
+  w[0] = None
+  w[1] = None
+  w[2] = None
+  w[3] = np.array([2, 2, -3, 3], dtype='float32').reshape([2, 2])
+  w[4] = np.array([2, 4, -1, 1], dtype='float32').reshape([2, 2])
+  w[5] = np.array([-2, 3, 3, -5], dtype='float32').reshape([2, 2])
+  b[0] = np.array([3, 4], dtype='float32').reshape([2])
+  b[1] = np.array([3, -3], dtype='float32').reshape([2])
+  b[2] = np.array([-1, -5], dtype='float32').reshape([2])
+  b[3] = np.array([0, -3], dtype='float32').reshape([2])
+  b[4] = np.array([-1, 0], dtype='float32').reshape([2])
+  b[5] = np.array([4, 1], dtype='float32').reshape([2])
+  # =========================================================
+  hx = np.array([0, 0], dtype='float32').reshape([2])
+  cx = np.array([0, 0], dtype='float32').reshape([2])
+  seq_x = [None] * 10
+  seq_x[0] = np.array([-5, 4], dtype='float32').reshape([2])
+  seq_x[1] = np.array([-4, 4], dtype='float32').reshape([2])
+  seq_x[2] = np.array([1, 2], dtype='float32').reshape([2])
+  seq_x[3] = np.array([-2, -1], dtype='float32').reshape([2])
+  seq_x[4] = np.array([0, 2], dtype='float32').reshape([2])
+  seq_x[5] = np.array([-3, -1], dtype='float32').reshape([2])
+  seq_x[6] = np.array([3, 1], dtype='float32').reshape([2])
+  seq_x[7] = np.array([-5, -4], dtype='float32').reshape([2])
+  seq_x[8] = np.array([-1, 3], dtype='float32').reshape([2])
+  seq_x[9] = np.array([-4, -3], dtype='float32').reshape([2])
+  # =========================================================
+  hy = np.array([-0.998948, -0.995195], dtype='float32').reshape([2])
+  cy = np.array([0, 0], dtype='float32').reshape([2])
+  seq_y = [None] * 10
+  seq_y[0] = np.array([-0.952544, -0.00179997], dtype='float32').reshape([2])
+  seq_y[1] = np.array([-0.998906, -0.125215], dtype='float32').reshape([2])
+  seq_y[2] = np.array([-0.244914, -0.590022], dtype='float32').reshape([2])
+  seq_y[3] = np.array([-0.929613, -0.994685], dtype='float32').reshape([2])
+  seq_y[4] = np.array([-0.154062, -0.252855], dtype='float32').reshape([2])
+  seq_y[5] = np.array([-0.918886, -0.987749], dtype='float32').reshape([2])
+  seq_y[6] = np.array([0.400333, -0.869267], dtype='float32').reshape([2])
+  seq_y[7] = np.array([-0.995206, -0.999966], dtype='float32').reshape([2])
+  seq_y[8] = np.array([-0.927997, -0.12594], dtype='float32').reshape([2])
+  seq_y[9] = np.array([-0.998948, -0.995195], dtype='float32').reshape([2])
+  # =========================================================
+  np_seq_y, np_hy, np_cy = run(gru_cell, w, b, seq_x, hx, cx)
+  for np_sy, sy in zip(np_seq_y, seq_y):
+    np.testing.assert_allclose(np_sy, sy, rtol=1e-6, atol=1e-6)
+  np.testing.assert_allclose(np_hy, hy, rtol=1e-6, atol=1e-6)
+  np.testing.assert_allclose(np_cy, cy, rtol=1e-6, atol=1e-6)
+  print("Verify GRU cell done!")
+
+
+def verify_lstm_cell():
+  # =========================================================
+  w = [None] * 8
+  b = [None] * 8
+  w[0] = None
+  w[1] = None
+  w[2] = None
+  w[3] = None
+  w[4] = np.array([2, 2, -3, 3], dtype='float32').reshape([2, 2])
+  w[5] = np.array([2, 4, -1, 1], dtype='float32').reshape([2, 2])
+  w[6] = np.array([-2, 3, 3, -5], dtype='float32').reshape([2, 2])
+  w[7] = np.array([3, 4, 3, -3], dtype='float32').reshape([2, 2])
+  b[0] = np.array([-1, -5], dtype='float32').reshape([2])
+  b[1] = np.array([0, -3], dtype='float32').reshape([2])
+  b[2] = np.array([-1, 0], dtype='float32').reshape([2])
+  b[3] = np.array([4, 1], dtype='float32').reshape([2])
+  b[4] = np.array([-5, 4], dtype='float32').reshape([2])
+  b[5] = np.array([-4, 4], dtype='float32').reshape([2])
+  b[6] = np.array([1, 2], dtype='float32').reshape([2])
+  b[7] = np.array([-2, -1], dtype='float32').reshape([2])
+  # =========================================================
+  hx = np.array([0, 0], dtype='float32').reshape([2])
+  cx = np.array([0, 0], dtype='float32').reshape([2])
+  seq_x = [None] * 10
+  seq_x[0] = np.array([0, 2], dtype='float32').reshape([2])
+  seq_x[1] = np.array([-3, -1], dtype='float32').reshape([2])
+  seq_x[2] = np.array([3, 1], dtype='float32').reshape([2])
+  seq_x[3] = np.array([-5, -4], dtype='float32').reshape([2])
+  seq_x[4] = np.array([-1, 3], dtype='float32').reshape([2])
+  seq_x[5] = np.array([-4, -3], dtype='float32').reshape([2])
+  seq_x[6] = np.array([4, -4], dtype='float32').reshape([2])
+  seq_x[7] = np.array([1, 1], dtype='float32').reshape([2])
+  seq_x[8] = np.array([-4, -2], dtype='float32').reshape([2])
+  seq_x[9] = np.array([3, 1], dtype='float32').reshape([2])
+  # =========================================================
+  hy = np.array([0.0470013, 0.355363], dtype='float32').reshape([2])
+  cy = np.array([0.0473509, 0.531963], dtype='float32').reshape([2])
+  seq_y = [None] * 10
+  seq_y[0] = np.array([0, 0.5491], dtype='float32').reshape([2])
+  seq_y[1] = np.array([-0.000248474, 0.00493969], dtype='float32').reshape([2])
+  seq_y[2] = np.array([0.0471861, 0.373676], dtype='float32').reshape([2])
+  seq_y[3] = np.array([-2.03695e-06, 0.000129828], dtype='float32').reshape([2])
+  seq_y[4] = np.array([-0.000507384, 0.682047], dtype='float32').reshape([2])
+  seq_y[5] = np.array([-0.00011725, 0.000419007], dtype='float32').reshape([2])
+  seq_y[6] = np.array([0.118247, -6.0058e-05], dtype='float32').reshape([2])
+  seq_y[7] = np.array([0.0120833, 0.307651], dtype='float32').reshape([2])
+  seq_y[8] = np.array([-2.3041e-05, 0.00197692], dtype='float32').reshape([2])
+  seq_y[9] = np.array([0.0470013, 0.355363], dtype='float32').reshape([2])
+  # =========================================================
+  np_seq_y, np_hy, np_cy = run(lstm_cell, w, b, seq_x, hx, cx)
+  for np_sy, sy in zip(np_seq_y, seq_y):
+    np.testing.assert_allclose(np_sy, sy, rtol=1e-6, atol=1e-6)
+  np.testing.assert_allclose(np_hy, hy, rtol=1e-6, atol=1e-6)
+  np.testing.assert_allclose(np_cy, cy, rtol=1e-6, atol=1e-6)
+  print("Verify LSTM cell done!")
+
+
 verify_relu_cell()
 verify_tanh_cell()
+verify_gru_cell()
+verify_lstm_cell()
